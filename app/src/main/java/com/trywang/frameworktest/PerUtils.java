255c1ca20765e91d280e0583e6ed3b525c4c67df
TryWang <wjfang_email@163.com>
2020-03-10 16:41:00 +0800

    test

--- app/src/main/java/com/trywang/frameworktest/PerUtils.java
+++ app/src/main/java/com/trywang/frameworktest/PerUtils.java
@@ -33,33 +33,36 @@ public class PerUtils {
 
     /**
      * 检查权限
+     *
      * @param context
      * @param permissions
      * @return true:已全部有权限   false:部分权限没有
      */
-    public static boolean checkPer(Context context,String[] permissions) {
-        return checkPermission(context,permissions).size() <= 0;
+    public static boolean checkPer(Context context, String[] permissions) {
+        return checkPermission(context, permissions).size() <= 0;
     }
 
     /**
      * 检查权限
+     *
      * @param context
      * @return true:已全部有权限   false:部分权限没有
      */
     public static boolean checkPermission(Context context) {
-        return checkPermission(context,PERMISSIONS).size() <= 0;
+        return checkPermission(context, PERMISSIONS).size() <= 0;
     }
 
     /**
      * 检查权限
+     *
      * @param context
      * @param perGroup 权限组
      * @return list长度  <= 0 表示权限都通过
      */
-    public static List<String> checkPermission(Context context, String[] perGroup){
+    public static List<String> checkPermission(Context context, String[] perGroup) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < perGroup.length; i++) {
-            if (ContextCompat.checkSelfPermission(context,perGroup[i])
+            if (ContextCompat.checkSelfPermission(context, perGroup[i])
                     == PackageManager.PERMISSION_DENIED) {
                 //没有权限
                 list.add(perGroup[i]);
@@ -68,15 +71,13 @@ public class PerUtils {
         return list;
     }
 
-    public static String getIMEI(Context context){
+    public static String getIMEI(Context context) {
         String imei = "";
         try {
             TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
-                if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_DENIED) {
-                    imei = tm.getDeviceId();
-                }
-            }else {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                imei = tm.getDeviceId();
+            } else {
                 Method method = tm.getClass().getMethod("getImei");
                 imei = (String) method.invoke(tm);
             }
